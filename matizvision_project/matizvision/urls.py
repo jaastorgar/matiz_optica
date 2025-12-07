fd7cf6a9a46fe03626e079f54a4189514cd5aff1
Javier <ja.astorgar@duocuc.cl>
2025-12-07 14:34:05 -0300

    creación de las urls principales y conectarla a la url madre.

--- matizvision_project/matizvision/urls.py
+++ matizvision_project/matizvision/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')), 
+    path('appointments/', include('appointments.urls')), 
+    path('shop/', include('shop.urls')),
+    path('avatars/', include('avatars.urls')),
 ]
