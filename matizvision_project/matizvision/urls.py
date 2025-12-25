1e3df271ed4c9ec2bddf262a372d2677b6648daa
Javier <ja.astorgar@duocuc.cl>
2025-12-25 19:27:06 -0300

    mejoras en la url de media.

--- matizvision_project/matizvision/urls.py
+++ matizvision_project/matizvision/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,8 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('avatars/', include('avatars.urls')),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
